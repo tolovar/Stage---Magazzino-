71770c7191aa21785b8d25aafcee9e737546bf15
tolovar <alessio.longo@itsrizzoli.it>
2025-05-13 16:04:37 +0200

    Update Missione.java

--- code/Missione.java
+++ code/Missione.java
@@ -18,26 +18,25 @@ public static void main(String[] args) {
 }
 
 public static void bubbleSort(int [] array) {
-	for(int i = 0; i < array.length; i++) {
-		boolean flag = false;
-		for(int j = 0; j < array.length-1; j++) {
-			//se l'elemento puntato da j è maggiore del successivo 
-			//i valori vengono scambiati tra loro
-			if(array[j]>array[j+1]) {
-				int k = array[j];
-				array[j] = array[j+1];
-				array[j+1] = k;
-				flag=true; 
-				//lo imposto come true per indicare che é avvenuto uno scambio
-			}
-		}
-		if(!flag) break; 
-		//se 'flag=false' significa che
-		//nell'ultima interazione non ci sono stati scambi:
-		//l'array è ordinato e il metodo può chiudersi
-	}
-	//stampo l'array dopo l'ordinamento
-
-	System.out.println("Array dopo l'ordinamento: ");
-	}
+for(int i = 0; i < array.length; i++) {
+boolean flag = false;
+for(int j = 0; j < array.length-1; j++) {
+//se l'elemento puntato da j è maggiore del successivo 
+//i valori vengono scambiati tra loro
+if(array[j]>array[j+1]) {
+int k = array[j];
+ array[j] = array[j+1];
+ array[j+1] = k;
+ flag=true; 
+ //lo imposto come true per indicare che é avvenuto uno scambio
+}
+}
+if(!flag) break; 
+//se 'flag=false' significa che 
+//nell'ultima interazione non ci sono più scambi:
+//l'array è ordinato e il metodo può chiudersi
+}
+//stampo l'array dopo l'ordinamento
+System.out.println("Array dopo l'ordinamento: ");
+}
 }
